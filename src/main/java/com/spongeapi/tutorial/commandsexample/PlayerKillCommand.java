270922f5415804f013163865dbc89f4faffeddea
Xakep_SDK <salimhon@gmail.com>
2017-10-17 22:18:00 +0600

    Fixes, improvements.

--- src/main/java/com/spongeapi/tutorial/commandsexample/PlayerKillCommand.java
+++ src/main/java/com/spongeapi/tutorial/commandsexample/PlayerKillCommand.java
@@ -8,6 +8,7 @@ import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.text.Text;
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -21,12 +22,12 @@ public class PlayerKillCommand implements CommandExecutor {
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        Optional<Player> playerOpt = args.getOne("player");
-        if (playerOpt.isPresent()) {
-            Player player = playerOpt.get();
-            Optional<Integer> timeOpt = args.getOne("time");
-            Task.builder().delay(timeOpt.orElse(0), TimeUnit.SECONDS).execute(() -> player.offer(Keys.HEALTH, 0d)).submit(plugin);
-        }
+        Player player = args.<Player>getOne("player").get();
+        Optional<Integer> timeOpt = args.getOne("time");
+        src.sendMessage(Text.of(player.getName() + " умрет через " + timeOpt.orElse(0) + " секунд"));
+        Task.builder().delay(timeOpt.orElse(0), TimeUnit.SECONDS)
+                .execute(() -> player.offer(Keys.HEALTH, 0d))
+                .submit(plugin);
         return CommandResult.empty();
     }
 }
